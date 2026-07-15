b6adcdb649baf913e4c170724412da329b24d302
Décio Luíz <decioluiz@msn.com>
2026-07-14 21:02:05 -0300

    Seção15/16 - Custom Serialization e ContentNegotiation

---
+++ RestWithASPNet10/RestWithASPNet10/Configurations/ContentNegotiationConfig.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Net.Http.Headers;
+
+namespace RestWithASPNet10.Configurations
+{
+    public static class ContentNegotiationConfig
+    {
+        public static IMvcBuilder AddContentNegotiation(this IMvcBuilder builder)
+        {
+            return builder.AddMvcOptions(options =>
+            {
+                options.RespectBrowserAcceptHeader = true; // Respect the Accept header
+                options.ReturnHttpNotAcceptable = true; // Return 406 Not Acceptable if the requested format is not supported
+
+                options.FormatterMappings.SetMediaTypeMappingForFormat("xml", MediaTypeHeaderValue.Parse("application/xml"));
+                options.FormatterMappings.SetMediaTypeMappingForFormat("json", MediaTypeHeaderValue.Parse("application/json"));
+            }).AddXmlSerializerFormatters();
+        }
+    }
+}
--- RestWithASPNet10/RestWithASPNet10/Data/DTO/V2/PersonDTO.cs
+++ RestWithASPNet10/RestWithASPNet10/Data/DTO/V2/PersonDTO.cs
@@ -1,12 +1,28 @@
-﻿namespace RestWithASPNet10.Data.DTO.V2
+﻿using RestWithASPNet10.JsonSerializers;
+using System.Text.Json.Serialization;
+
+namespace RestWithASPNet10.Data.DTO.V2
 {
     public class PersonDTO
     {
+        //[JsonPropertyOrder(1)]
+        //[JsonPropertyName("code")]
         public long Id { get; set; }
+        //[JsonPropertyOrder(2)]
+        //[JsonPropertyName("first_name")]
         public string FirstName { get; set; }
+        //[JsonPropertyOrder(3)]
+        //[JsonPropertyName("last_name")]
+        //[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string LastName { get; set; }
+        //[JsonPropertyOrder(4)]
+        //[JsonConverter(typeof(GenderSerializer))]
         public string Gender { get; set; }
+        //[JsonPropertyOrder(6)]
         public string Address { get; set; }
+        //[JsonPropertyOrder(5)]
+        //[JsonConverter(typeof(DateSerializer))]
+        //[JsonIgnore]
         public DateTime? BirthDate { get; set; }
     }
 }
